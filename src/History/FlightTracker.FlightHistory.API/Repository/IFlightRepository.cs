2e902f93cbfb24405528a2c2218c800489f6e01d
Zac Jones <zacjones91@gmail.com>
2022-03-31 16:23:27 -0500

    Add repository

---
+++ src/History/FlightTracker.FlightHistory.API/Repository/IFlightRepository.cs
@@ -0,0 +1,14 @@
+﻿using FlightTracker.Common;
+using FlightTracker.FlightHistory.API.Models;
+
+namespace FlightTracker.FlightHistory.API.Repository
+{
+    public interface IFlightRepository
+    {
+        public Task<int> AddFlight(Flight flight);
+
+        public Task<bool> AddFlightStatus(ADSBPing adsPing, int flightId);
+
+        public Task<IEnumerable<ADSBPing>> GetTrackByFlightId(int flightId);
+    }
+}
