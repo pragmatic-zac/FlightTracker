f687c56f86da64ca53b28da76e918777456f8e0b
Zac Jones <zacjones91@gmail.com>
2022-03-31 16:37:41 -0500

    Wire up consumer worker and new startup

---
+++ src/History/FlightTracker.FlightHistory.API/Workers/HistoryIngestWorker.cs
@@ -0,0 +1,24 @@
+﻿using FlightTracker.Common;
+using MassTransit;
+
+namespace FlightTracker.FlightHistory.API.Workers
+{
+    public class HistoryIngestWorker : IConsumer<PiAwareLog>
+    {
+        readonly ILogger<HistoryIngestWorker> _logger;
+
+        public HistoryIngestWorker(ILogger<HistoryIngestWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<PiAwareLog> context)
+        {
+            _logger.LogInformation("Recieved message");
+
+            var test = context;
+
+            return Task.CompletedTask;
+        }
+    }
+}
