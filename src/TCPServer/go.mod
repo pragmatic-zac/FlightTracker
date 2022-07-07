d7842c26b5ad5f1775b7beb1111c74118a299d34
Zac Jones <zacjones91@gmail.com>
2022-07-06 21:45:48 -0500

    WIP

--- src/TCPServer/go.mod
+++ src/TCPServer/go.mod
@@ -1,3 +1,5 @@
 module github.com/zacjones91/FlightTrackerIngest
 
 go 1.18
+
+require github.com/rabbitmq/amqp091-go v1.3.4
--- src/TCPServer/main.go
+++ src/TCPServer/main.go
@@ -4,10 +4,13 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"strconv"
 	"strings"
 	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 var count = 0
@@ -88,7 +91,46 @@ func handleConnection(c net.Conn) {
 	c.Close()
 }
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
 func main() {
+	// RabbitMQ Setup
+	conn, err := amqp.Dial("amqp://guest:guest@track-rabbitmq:5672")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+
+	// TCP
 	println("STARTING TCP")
 	l, err := net.Listen("tcp4", ":2100")
 	if err != nil {
